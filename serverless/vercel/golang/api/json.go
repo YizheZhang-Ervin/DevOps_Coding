2a9dcb8ffcc1ec1e5300bee7e0a774947c9a595c
ervin <yizhezhang97@sina.com>
2023-12-30 16:42:03 +0800

    rm codes

--- serverless/vercel/golang/api/json.go
+++ serverless/vercel/golang/api/json.go
@@ -10,10 +10,7 @@ func Json(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	resp := make(map[string]string)
-	resp["message"] = "Hello World from Go! 👋"
-	resp["language"] = "go"
-	resp["cloud"] = "Hosted on Vercel! ▲"
-	resp["github"] = "https://github.com/riccardogiorato/template-go-vercel/blob/main/api/json.go"
+	resp["message"] = "Hello"
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
 		fmt.Printf("Error happened in JSON marshal. Err: %s", err)
